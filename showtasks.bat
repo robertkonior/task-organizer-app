7629ef269df9530a66d29590ace4c8715ab0d221
robertkonior <praca.robert.konior@gmail.com>
2018-01-04 17:24:27 +0000

    17.4/0.1

---
+++ showtasks.bat
@@ -0,0 +1,17 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == 0 goto runbrowser
+echo.
+echo GRADLEW BUILD has errors - breaking work
+goto fail
+
+:runbrowser
+chrome http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto end
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
