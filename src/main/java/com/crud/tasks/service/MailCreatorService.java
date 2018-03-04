af5ed6ec6e2297ada439e9af27acf26093b13c4e
robertkonior <praca.robert.konior@gmail.com>
2018-03-04 22:03:26 +0100

    24.2/0.1

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,25 @@
+package com.crud.tasks.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url","http://localhost:8080/v1");
+        context.setVariable("button","Visit website");
+        return templateEngine.process("mail/created_trello-card-mail", context);
+    }
+}
