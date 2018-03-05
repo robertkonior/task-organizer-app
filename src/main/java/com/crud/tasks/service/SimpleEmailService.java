a18384c1e08c4b959908f833cdc8ea1108ff46fb
robertkonior <praca.robert.konior@gmail.com>
2018-03-05 17:40:36 +0100

    24.2/2

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -38,7 +38,7 @@ public class SimpleEmailService {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(mail.getMailTo());
             messageHelper.setSubject(mail.getSubject());
-            messageHelper.setText(mail.getMessage(),true);
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()),true);
         };
     }
 
